b6c9e3a574afbcc009de441c11a2da619c44b5d5
sindhuja713 <157420890+sindhuja713@users.noreply.github.com>
2024-05-02 17:55:40 -0500

    first commit

---
+++ pricing-table/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Sub from './components/subscription/Sub';
+import SubList from './components/subscription/SubList';
+
+function App() {
+  return (
+    <div >
+      <Sub />
+      <SubList />
+    </div>
+  );
+}
+
+export default App;
